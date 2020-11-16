9d7ca0d4035cf8a9712d2197c2aeff18cc441ac8
adilson_chameia <adilsokchameia@outlook.com>
2020-11-16 13:18:16 +0100

    Initial commit

---
+++ app/src/main/java/com/example/videoplayer/PlayerVideo.java
@@ -0,0 +1,36 @@
+package com.example.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerVideo extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_video);
+
+        videoView = findViewById(R.id.videoView);
+
+        //esconder barra status bar e modo fullscreen
+        View decorView = getWindow().getDecorView();
+        int uiOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOpcoes);
+
+        //esconder actionbar
+        getSupportActionBar().hide();
+
+        //executar video - controles padroes do android
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+    }
+
+
+}
